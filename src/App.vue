1fe1d55fd04f65ab4dd9435f94e5e27118c7c971
Caio <caio_93@live.com>
2024-03-05 11:36:19 +1100

    ..

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  {{ msg }}
+
 </template>
+
+<script setup lang="ts">
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { ref } from 'vue';
+const msg = ref('Hello Vue 3 + Vite + Firebase!');
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBo5mlbSmaf0aSempfeMTY8RQeSBfgkQlc",
+  authDomain: "testing-62acb.firebaseapp.com",
+  projectId: "testing-62acb",
+  storageBucket: "testing-62acb.appspot.com",
+  messagingSenderId: "195165267631",
+  appId: "1:195165267631:web:383052269d80aea630165a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+msg.value = JSON.stringify(app)
+</script>
 
 <style scoped>
 .logo {
@@ -21,9 +33,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
